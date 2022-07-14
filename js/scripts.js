1b35ae71cbb02296cba884e10b2d555bc784d70b
Jose Gonzalez <joseraul88@gmail.com>
2022-07-14 15:22:03 -0500

    Mobile nav closes when click on link

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,16 @@
 const toggleBtn = document.querySelector(".toggle-button");
 const mobileNav = document.querySelector(".mobile-nav-container");
 const backdrop = document.querySelector(".backdrop");
+const mobileNavEls = document.querySelectorAll(
+  ".mobile-nav-container .nav-link"
+);
+
+mobileNavEls.forEach(function (link) {
+  link.addEventListener("click", () => {
+    mobileNav.classList.remove("open");
+    backdrop.style.display = "none";
+  });
+});
 
 toggleBtn.addEventListener("click", function openMobileNav() {
   mobileNav.classList.add("open");
